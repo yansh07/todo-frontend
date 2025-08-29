bd2c20f890db36be2360644eb30f02ce8e5d5e01
pksingh <pksingh69313@gmail.com>
2025-08-29 15:22:13 +0530

    Added navbar, login and sign up buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,22 @@ function Navbar() {
     <div className="bg-gradient-to-r from-[#413d3d] to-[#a4a3a6] flex flex-row">
       <div>
         {/* Logo + Title */}
-        <div className="top-3 left-4 fixed p-1 flex items-center gap-2">
-          <i className="fa-solid fa-file-word text-2xl text-gray-50"></i>
-          <span className="text-2xl font-[satoshi] font-extrabold text-gray-50">
+        <div className="top-3 left-4 fixed p-1 flex items-center  md:ml-24 md:mt-4 lg:ml-52 lg:mt-8 xl:ml-92">
+          <i className="fa-solid fa-file-word text-3xl md:text-4xl xl:text-5xl text-gray-50"></i>
+          <span className="text-3xl font-[satoshi] font-extrabold text-gray-50 md:text-4xl xl:text-5xl">
             PlanIt
           </span>
         </div>
 
         {/* Buttons */}
-        <div className="flex flex-row gap-6 fixed right-4 top-3">
+        <div className="flex flex-row gap-4 md:gap-6  fixed right-4 top-3 md:mr-24 md:mt-4 lg:mr-52  lg:mt-8 xl:mr-92">
           {/* Login Button */}
           <a href="#">
             <button
               className="font-[satoshi] text-lg font-semibold text-white px-4 py-2 rounded-lg 
             bg-gradient-to-r from-white/50 to-gray-700
             shadow-md hover:shadow-xl hover:scale-105 
-            transition-all duration-300 ease-in-out backdrop:blur-3xl"
+            transition-all duration-300 ease-in-out backdrop:blur-3xl xl:text-xl"
             >
               Login
             </button>
@@ -32,7 +32,7 @@ function Navbar() {
               className="font-[satoshi] text-lg font-semibold text-white px-4 py-2 rounded-lg 
             bg-gradient-to-r from-white/50 to-gray-700 
             shadow-md hover:shadow-xl hover:scale-105 
-            transition-all duration-300 ease-in-out backdrop:blur-3xl"
+            transition-all duration-300 ease-in-out backdrop:blur-3xl xl:text-xl"
             >
               Sign up
             </button>
