d2493a724484a233017bbf314954edef4e6f80fb
pksingh <pksingh69313@gmail.com>
2025-08-30 15:49:25 +0530

    Finished navbar, added shortcut, profile picture icons

--- src/components/Usernav.jsx
+++ src/components/Usernav.jsx
@@ -4,7 +4,7 @@ import "/src/index.css";
 
 function Usernav() {
   return (
-    <div className="bg-gradient-to-br from-[#060010] to-slate-600 p-4">
+    <div className="bg-gradient-to-br from-[#060010] to-slate-600 p-2">
       <div className="flex items-center space-x-2 px-0 md:px-18 lg:px-42 xl:px-24">
         <i className="fa-solid fa-file-word text-3xl md:text-4xl xl:text-5xl text-blue-400"></i>
         <span className="text-3xl font-[satoshi] font-extrabold text-blue-400 md:text-4xl xl:text-5xl ">
@@ -13,13 +13,15 @@ function Usernav() {
         <div className="ml-20 md:ml-72 lg:ml-110 xl:ml-200">
           <div className="relative group inline-block">
             <FilePlus
-              className="text-blue-400 cursor-pointer transition-transform duration-200 group-hover:scale-110 group-hover:rotate-6"
-              size={34}
+              className="text-blue-400 cursor-pointer transition-transform duration-200 group-hover:scale-110 group-hover:rotate-6 w-10 h-10 mt-2"
             />
-            <span className="absolute top-full left-1/2 -translate-x-1/2 mt-2 w-max px-2 py-1 text-sm text-white bg-gray-800 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <span className="absolute top-2 left-1/2 -translate-x-1/2 mt-4 w-max px-2 text-sm text-white bg-gray-800 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
               Add a new note
             </span>
           </div>
+        </div>
+        <div className="ml-6 md:ml-16">
+            <div className="profilecircle border-2 w-11 h-11 cursor-pointer rounded-full border-blue-400"></div>
         </div>
       </div>
     </div>
