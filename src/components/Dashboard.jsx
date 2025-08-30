f21841a58389490c9d156ade4fe80fe78e4b9b6b
pksingh <pksingh69313@gmail.com>
2025-08-30 16:05:47 +0530

    Finished navbar, added shortcut, profile picture icons

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,8 +3,11 @@ import Usernav from './Usernav'
 
 function Dashboard() {
   return (
-    <div>
+    <div className='bg-gradient-to-tr from-[#060010] to-slate-600 min-h-screen'>
       <Usernav />
+      <div className='px-6 py-4 md:px-22 md:py-10 lg:px-44'>
+        <h1 className='font-[satoshi] text-gray-50 font-bold text-2xl'>Hi User 👋</h1>
+      </div>
     </div>
   )
 }
