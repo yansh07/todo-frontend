1be321627b092b2e6bbe63dbb34e7b0b7772a691
pksingh <pksingh69313@gmail.com>
2025-09-02 17:27:48 +0530

    Add footer, adjust responsiveness on sm and xl screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,53 +2,60 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-[#100227] flex flex-col relative z-50 px-4 md:px-8 pt-2 md:py-2 lg:px-14 lg:py-6 xl:px-32 xl:py-4">
-      <a
-        href="https://github.com/yansh07/todo-frontend"
-        className="text-gray-50  hover:text-blue-400 transition-all duration-300 ml-3 mt-3 font-semibold text-2xl"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        PlanIt
-      </a>
-      <p className="font-[satoshi] text-gray-400 font-medium text-lg ml-3 hover:text-white mr-10">
-        A minimalist canvas to save your plans, and moments.
-      </p>
+    <div className="bg-[#100227] flex flex-col relative z-50 px-4 md:px-8 pt-6 md:py-6 lg:px-70 lg:py-6 xl:px-117 xl:py-8 lg:flex-row">
       <div>
-        <h1 className="font-[satoshi] text-gray-100 text-xl font-bold ml-3 mt-6">
+        <a
+          href="https://github.com/yansh07/todo-frontend"
+          className="text-gray-50  hover:text-blue-400 transition-all duration-300 ml-3 mt-3 font-semibold text-2xl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PlanIt
+        </a>
+        <p className="font-[satoshi] text-gray-400 font-medium text-lg ml-3 hover:text-white mr-10 lg:max-w-3xs">
+          A minimalist canvas to save your plans, and moments.
+        </p>
+      </div>
+      <div className="xl:mb-28 lg:mb-28 lg:ml-6">
+        <h1 className="font-[satoshi] text-gray-100 text-xl font-bold ml-3 mt-6 lg:mt-0">
           Connect
         </h1>
         <ul className="font-[satoshi] text-gray-400 ml-3 mt-2 text-lg">
           <li className="font-medium  hover:text-white">
             <a href="https://github.com/yansh07">
-              <i class="fa-brands fa-github text-white/80 hover:text-white "></i> - GitHub
+              <i class="fa-brands fa-github text-white/80 hover:text-white "></i>{" "}
+              - GitHub
             </a>
           </li>
           <li className="font-medium  hover:text-white">
             <a href="https://x.com/yansh_08">
-              <i class="fa-brands fa-x-twitter text-white/80 hover:text-white"></i> - X(Twitter)
+              <i class="fa-brands fa-x-twitter text-white/80 hover:text-white"></i>{" "}
+              - X(Twitter)
             </a>
           </li>
           <li className="font-medium  hover:text-white">
             <a href="mailto:pksingh69313@gmail.com">
-              <i class="fa-solid fa-inbox text-white/80 hover:text-white"></i> - Email
+              <i class="fa-solid fa-inbox text-white/80 hover:text-white"></i> -
+              Email
             </a>
           </li>
         </ul>
       </div>
       <div>
-        <div className="border-t border-gray-500 ml-3 mr-3 mt-4"></div>
-      </div>
-      <div className="font-[satoshi] text-gray-400 mt-3 mb-6">
-        <p className="text-sm ml-3">© 2025 PlanIt: NotesApp.</p>
-        <p className="font-medium text-sm ml-3 hover:text-white">
-          <a
-            href="https://github.com/yansh07
+        <div>
+          <div className="border-t border-gray-500 ml-3 mr-3 mt-4 lg:absolute lg:bottom-20 lg:left-68 lg:px-58 xl:px-56 xl:ml-52"></div>
+        </div>
+        <div className="font-[satoshi] text-gray-400 mt-3 mb-6 lg:absolute lg:bottom-0 lg:left-96 lg:px-10 xl:px-52">
+          <p className="text-sm ml-3">© 2025 PlanIt: NotesApp.</p>
+          <p className="font-medium text-sm ml-3 hover:text-white">
+            <a
+              href="https://github.com/yansh07
 "
-          >
-            Made with 🤍 by Priyanshu
-          </a>
-        </p>
+            >
+              Made with 🤍 by Priyanshu
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,6 +3,8 @@ import { User, Plus } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import AddNote from "./AddNote";
 import AboutMeInput from "./Aboutme";
+import Footer from "./Footer";
+
 
 function Profile() {
   const [notes, setNotes] = useState([]);
@@ -23,7 +25,7 @@ function Profile() {
   return (
     // profile card
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-violet-900  flex flex-col">
-      <div className="flex flex-row gap-10 px-8 py-8 md:px-24">
+      <div className="flex flex-row gap-10 px-8 py-8 md:px-38 lg:px-72 xl:px-120 xl:mt-4">
         <div className="w-20 h-20 md:w-20 md:h-20 rounded-full bg-gradient-to-r from-pink-400 via-purple-400 to-cyan-400 p-[2px] cursor-pointer group-hover:scale-110 transition-transform duration-300">
           <div className="w-full h-full bg-gray-800/50 backdrop-blur-sm rounded-full flex items-center justify-center">
             <User className="w-10 h-10 text-white" />
@@ -45,8 +47,8 @@ function Profile() {
       <div className="px-8 mb-6">
         <AboutMeInput />
       </div>
-      <div className="font-[satoshi] font-medium  px-8">
-        <div className="flex flex-row gap-8">
+      <div className="font-[satoshi] font-medium  px-8 md:px-40 lg:px-72 xl:px-120">
+        <div className="flex flex-row gap-8 md:gap-48">
           {/* Add Note Button */}
           <div className="flex justify-center">
             <button
@@ -54,20 +56,20 @@ function Profile() {
               className="group relative overflow-hidden bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600 text-white font-bold py-3 px-3 rounded-2xl shadow-2xl shadow-purple-500/30 transition-all duration-300 hover:scale-105 hover:shadow-purple-500/50 font-[satoshi]"
             >
               <div className="flex items-center gap-2">
-                <Plus className="w-6 h-6" />
+                {/* <Plus className="w-6 h-6" /> */}
                 <span className="text-md">Add Picture</span>
               </div>
               {/* Shine effect */}
               <div className="absolute inset-0 -top-full bg-gradient-to-b from-transparent via-white/20 to-transparent group-hover:top-full transition-all duration-1000"></div>
             </button>
           </div>
-          <div className="flex justify-center">
+          <div className="flex justify-center ml-6">
             <button
               onClick={() => navigate("/add-note")}
-              className="group relative overflow-hidden bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600 text-white font-bold py-3 px-3 rounded-2xl shadow-2xl shadow-purple-500/30 transition-all duration-300 hover:scale-105 hover:shadow-purple-500/50 font-[satoshi]"
+              className="xl:py-4 group relative overflow-hidden bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600 text-white font-bold py-3 px-6 rounded-2xl shadow-2xl shadow-purple-500/30 transition-all duration-300 hover:scale-105 hover:shadow-purple-500/50 font-[satoshi]"
             >
               <div className="flex items-center gap-2">
-                <Plus className="w-6 h-6" />
+                {/* <Plus className="w-6 h-6" /> */}
                 <span className="text-md">New Note</span>
               </div>
               {/* Shine effect */}
@@ -75,18 +77,18 @@ function Profile() {
             </button>
           </div>
         </div>
-        <div className="flex flex-row gap-12">
-          <h2 className="px-3 py-2 rounded-2xl border-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600 text-white text-xl mt-6">
+        <div className="flex flex-row gap-12 md:gap-52">
+          <h2 className="px-3 py-3 xl:py-4 rounded-2xl border-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600 text-white text-md font-semibold mt-6 shadow-2xl shadow-purple-500/30 transition-all duration-300 hover:scale-105 hover:shadow-purple-500/50">
           Total notes: {totalNotes}
         </h2>
-        <h2 className="px-6 py-2 rounded-2xl border-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600 text-white text-xl mt-6">
+        <h2 className="px-8 py-3 xl:py-4 md:pt-5 rounded-2xl border-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 hover:from-pink-600 hover:via-purple-600 hover:to-indigo-600 text-white text-md font-semibold mt-6 shadow-2xl shadow-purple-500/30 transition-all duration-300 hover:scale-105 hover:shadow-purple-500/50">
           Logout
         </h2>
         </div>
       </div>
       {/*Notes Preview Section */}
-      <div className="flex-1 px-8 py-6 md:px-24">
-        <h2 className="text-xl font-medium  text-gray-100 mb-4">
+      <div className="flex-1 px-8 py-6 md:px-40 md:mt-4 lg:px-72 xl:px-120">
+        <h2 className="text-xl font-medium  text-gray-100 mb-4 lg:text-3xl lg:font-semibold">
           Latest Notes
         </h2>
 
@@ -112,6 +114,7 @@ function Profile() {
           </p>
         )}
       </div>
+      <Footer />
     </div>
   );
 }
