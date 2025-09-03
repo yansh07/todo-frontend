c9a662777fb4e378c021436d5ebe46cb7178dfc5
pksingh <pksingh69313@gmail.com>
2025-09-03 11:17:32 +0530

    fix:adjust navbar padding on xl screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import GreetingComponent from './Greeting';
 
 function Navbar() {
   return (
-    <div className="bg-transparent flex flex-row top-6 left-2 xl:left-0 fixed p-1 items-center md:ml-24 md:mt-4 lg:ml-52 lg:mt-8 xl:ml-92">
+    <div className="bg-transparent flex flex-row top-6 left-1 xl:left-0 fixed p-1 items-center md:ml-24 md:mt-4 lg:ml-52 lg:mt-8 xl:ml-96 xl:px-18">
       {/* Brand & Greeting Section */}
-      <div className="flex items-center space-x-46 md:space-x-92 lg:space-x-60 xl:space-x-72">
+      <div className="flex items-center space-x-44 md:space-x-92 lg:space-x-60 xl:space-x-72">
         <div className="flex items-center space-x-2">
           <i className="fa-solid fa-file-word text-3xl md:text-4xl xl:text-5xl text-yellow-500"></i>
           <span className="text-3xl font-[satoshi] font-extrabold text-yellow-500 md:text-4xl xl:text-5xl ">
